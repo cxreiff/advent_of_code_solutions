5815078c8adbedaa54cf3e0a217ec445ec2b79fa
cxreiff <dev@cooperreiff.com>
2023-01-15 08:02:18 +0000

    exercises 12,13,14, scaffolding for rest

---
+++ 2022/day_15/src/main.rs
@@ -0,0 +1,14 @@
+use day_15::part_1;
+use day_15::part_2;
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let input_path = &args[1];
+    let input = fs::read_to_string(input_path)
+        .unwrap_or_else(|_| panic!("Should have an input file at path: {}", input_path));
+
+    println!("{}", part_1(&input));
+    println!("{}", part_2(&input));
+}
